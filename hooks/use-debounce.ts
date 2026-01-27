5002adc3e542cf20f76fc8f6a6ad5a5d668a6cc1
CoderPapi14 <sbist738@gmail.com>
2026-01-27 17:39:18 +0530

    Increase default debounce delay to 1000ms
    The default delay for the useDebounce hook has been changed from 500ms to 1000ms to provide a longer debounce interval.

--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce<T>(value: T, delay: number = 500): T {
+export function useDebounce<T>(value: T, delay: number = 1000): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
